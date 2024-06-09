5c2005fb3dbac7b362f4549f6fdfb5bfb424dc3e
Ana Clara Bergamini Delgado <ana4ew.br@gmail.com>
2024-06-09 15:12:58 -0300

    Created: Spring Initializr

---
+++ src/main/java/br/edu/fag/productRegistration/ProductRegistrationApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fag.productRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/fag/productRegistration/ProductRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fag.productRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
