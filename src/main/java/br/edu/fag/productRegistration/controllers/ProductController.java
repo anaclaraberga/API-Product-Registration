37ab64d5269bce3993872b5a2c7885e96e8d6650
Ana Clara Bergamini Delgado <ana4ew.br@gmail.com>
2024-06-13 19:10:52 -0300

    Updated: Controller

--- src/main/java/br/edu/fag/productRegistration/controllers/ProductController.java
+++ src/main/java/br/edu/fag/productRegistration/controllers/ProductController.java
@@ -1,18 +1,29 @@
 package br.edu.fag.productRegistration.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.fag.productRegistration.entities.ProductEntity;
+import br.edu.fag.productRegistration.repository.ProductRepository;
 import lombok.Data;
 
 @Data
 @RestController
-@RequestMapping
+@RequestMapping("/home")
 public class ProductController {
 
-    @GetMapping
-    public void getAll() {
+    @Autowired
+    private ProductRepository repository;
 
+    @GetMapping
+    public List<ProductEntity> getAll() {
+
+        List<ProductEntity> product = repository.findAll();
+
+        return product;
     }
 }
--- src/main/java/br/edu/fag/productRegistration/entities/ProductEntity.java
+++ src/main/java/br/edu/fag/productRegistration/entities/ProductEntity.java
@@ -1,9 +1,28 @@
 package br.edu.fag.productRegistration.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Table(name="products")
 @Entity(name="products")
+@Data
 public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false)
+    private Integer price;
 }
