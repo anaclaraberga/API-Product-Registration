3dcc8127f397240810ffb09527124d3fa335c3e8
Ana Clara Bergamini Delgado <ana4ew.br@gmail.com>
2024-06-16 18:29:02 -0300

    Updated: Product Controller CRUD

--- src/main/java/br/edu/fag/productRegistration/controllers/ProductController.java
+++ src/main/java/br/edu/fag/productRegistration/controllers/ProductController.java
@@ -4,25 +4,36 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.fag.productRegistration.RequestDTO.ProductRequestDTO;
+import br.edu.fag.productRegistration.ResponseDTO.ProductResponseDTO;
 import br.edu.fag.productRegistration.entities.ProductEntity;
 import br.edu.fag.productRegistration.repository.ProductRepository;
 import lombok.Data;
 
 @Data
 @RestController
-@RequestMapping("/home")
+@RequestMapping("/product")
 public class ProductController {
 
     @Autowired
     private ProductRepository repository;
 
-    @GetMapping
-    public List<ProductEntity> getAll() {
+    @PostMapping
+    public void createProduct(@RequestBody ProductRequestDTO dto) {
+        ProductEntity product = new ProductEntity(dto);
 
-        List<ProductEntity> product = repository.findAll();
+        repository.save(product);
+    }
+
+    @GetMapping
+    public List<ProductResponseDTO> getAll() {
+
+        List<ProductResponseDTO> product = repository.findAll().stream().map(ProductResponseDTO::new).toList();
 
         return product;
     }
