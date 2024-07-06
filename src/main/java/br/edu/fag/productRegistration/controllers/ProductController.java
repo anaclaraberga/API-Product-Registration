a36f8f46d6cf627f67af9596aa0d05be812393a5
Ana Clara <ana4ew.br@gmail.com>
2024-07-06 16:22:27 -0300

    Created: Cross Configuration

--- src/main/java/br/edu/fag/productRegistration/controllers/ProductController.java
+++ src/main/java/br/edu/fag/productRegistration/controllers/ProductController.java
@@ -3,6 +3,7 @@ package br.edu.fag.productRegistration.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +24,7 @@ public class ProductController {
     @Autowired
     private ProductRepository repository;
 
+    @CrossOrigin(origins="*", allowedHeaders="*")
     @PostMapping
     public ProductEntity createProduct(@RequestBody ProductRequestDTO dto) {
         ProductEntity product = new ProductEntity(dto);
@@ -30,8 +32,9 @@ public class ProductController {
         return repository.save(product);
     }
 
+    @CrossOrigin(origins="*", allowedHeaders="*")
     @GetMapping
-    public List<ProductResponseDTO> getAll() {
+    public List<ProductResponseDTO> getAllProducts() {
 
         List<ProductResponseDTO> product = repository.findAll().stream().map(ProductResponseDTO::new).toList();
 
