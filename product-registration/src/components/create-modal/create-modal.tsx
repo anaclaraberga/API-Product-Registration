3d492ebfb0a0ffa4bc2dc5231be48a3a46a27aab
Ana Clara <ana4ew.br@gmail.com>
2024-08-19 21:33:13 -0300

    Fixed: Natural Number to Decimal Number

--- product-registration/src/components/create-modal/create-modal.tsx
+++ product-registration/src/components/create-modal/create-modal.tsx
@@ -3,12 +3,13 @@ import { useProductDataMutate } from "../../hooks/useProductDataMutate";
 import { ProductData } from "../../interface/ProductData";
 
 import "./modal.css";
+import React from "react";
 
 interface InputProps {
     label: string,
     value: string | number,
     updateValue(value: any): void
-}   
+}
 
 interface ModalProps {
     closeModal(): void
@@ -23,37 +24,38 @@ const Input = ({ label, value, updateValue }: InputProps) => {
     )
 }
 
-export function CreateModal({ closeModal } : ModalProps){
-const [title, setTitle] = useState("");
-const [price, setPrice] = useState(0);
-const [image, setImage] = useState("");
-const { mutate, isSuccess, isPending } = useProductDataMutate();
+export function CreateModal({ closeModal }: ModalProps) {
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = React.useState<number>(0);
+    const [image, setImage] = useState("");
+    const { mutate, isSuccess, isPending } = useProductDataMutate();
 
-const submit = () => {
-    const productData: ProductData = {
-        title,
-        price,
-        image
+    const submit = () => {
+        const productData: ProductData = {
+            title,
+            price: Number(price),
+            image
+        }
+
+        mutate(productData)
     }
-    mutate(productData)
-}
 
     useEffect(() => {
-        if(!isSuccess) return 
-            closeModal();
-}, [isSuccess])
+        if (!isSuccess) return
+        closeModal();
+    }, [isSuccess])
 
-    return(
+    return (
         <div className="modal-overlay">
             <div className="modal-body">
                 <h2>Cadastre um novo item no cardápio</h2>
                 <form className="input-container">
-                    <Input label="Nome do Produto" value={title}updateValue={setTitle}></Input>
-                    <Input label="Valor em R$" value={price}updateValue={setPrice}></Input>
-                    <Input label="Link da Imagem" value={image}updateValue={setImage}></Input>
+                    <Input label="Nome do Produto" value={title} updateValue={setTitle}></Input>
+                    <Input label="Valor em R$" value={price} updateValue={setPrice}></Input>
+                    <Input label="Link da Imagem" value={image} updateValue={setImage}></Input>
                 </form>
                 <button onClick={submit} className="btn-secondary">
-                    {isPending ? 'postando...' : 'postar'}
+                    {isPending ? 'Adicionando...' : 'Adicionar'}
                 </button>
             </div>
         </div>
