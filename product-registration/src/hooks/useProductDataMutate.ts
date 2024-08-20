cfdb25be1be1ac1f31c272402eeb3f36ef66dd0a
Ana Clara <ana4ew.br@gmail.com>
2024-08-19 21:34:33 -0300

    Fixed: Post Data

--- product-registration/src/hooks/useProductDataMutate.ts
+++ product-registration/src/hooks/useProductDataMutate.ts
@@ -5,7 +5,7 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 const API_URL = "http://localhost:8080";
 
 const postData = async (data: ProductData): AxiosPromise<any> => {
-    const response = axios.post(API_URL + '/product', data);
+    const response = await axios.post(API_URL + '/product', data);
     return response;
 }
 
